8fb50210fccdf2ca073a49fb293048813dc79583
= <=>
2021-06-07 02:14:56 -0600

    Able to sort by rating and date

--- src/app/animes/animes.component.ts
+++ src/app/animes/animes.component.ts
@@ -8,8 +8,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AnimesComponent implements OnInit {
   animes!: string;
+  sortOptions = [
+    {
+      display: 'Average Rating',
+      value: 'averageRating'
+    }, 
+    {
+      display: 'Start Date',
+      value: 'startDate'
+    }, 
+  ]
 
   constructor(private http: HttpClient) { }
+
+  onSortOptionChange(eventValue: Event) {
+    this.http
+    .get<any>(`https://kitsu.io/api/edge/anime?sort=-${(<HTMLInputElement>eventValue.target).value}`)
+    .subscribe(
+      data => {
+        console.log('Success: ', data);
+        this.animes = data.data;
+      },
+      error => {
+        console.log('Error: ', error);
+      });
+    console.log((<HTMLInputElement>eventValue.target).value);
+  }
 
   ngOnInit() {
     this.http
