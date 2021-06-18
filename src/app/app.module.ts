2fbbb2d076fe1db892a8a636c4445445a7151306
= <=>
2021-06-18 03:54:18 -0600

    Added Bootstrap CSS framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimeComponent } from './anime/anime.component';
 import { AnimesComponent } from './animes/animes.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DevlogModalComponent } from './devlog-modal/devlog-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimeComponent,
     AnimesComponent,
+    DevlogModalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
